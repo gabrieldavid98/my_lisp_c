385a8016302949bbcca3b51ec9dcb64e05bd6701
Gabriel Mendoza <gabrieldavid_98@hotmail.com>
2025-06-22 22:14:15 -0500

    add basic evaluation

--- main.c
+++ main.c
@@ -28,6 +28,87 @@ void add_history(char* unused) {}
 
 #endif // _WIN32
 
+long pow_l(long x, long y)
+{
+    if (y == 0)
+    {
+        return 1;
+    }
+
+    long res = x;
+    for (int i = 1; i < y; i++)
+    {
+        res *= res;
+    }
+
+    return res;
+}
+
+long min_l(long x, long y)
+{
+    if (x < y)
+    {
+        return x;
+    }
+
+    return y;
+}
+
+long max_l(long x, long y)
+{
+    if (x > y)
+    {
+        return x;
+    }
+
+    return y;
+}
+
+long eval_op(long x, char* op, long y)
+{
+    if (strcmp(op, "+") == 0 || strcmp(op, "add") == 0) return x + y;
+    if (strcmp(op, "-") == 0 || strcmp(op, "sub") == 0) return x - y;
+    if (strcmp(op, "*") == 0 || strcmp(op, "mul") == 0) return x * y;
+    if (strcmp(op, "/") == 0 || strcmp(op, "div") == 0) return x / y;
+    if (strcmp(op, "%") == 0 || strcmp(op, "mod") == 0) return x % y;
+    if (strcmp(op, "^") == 0) return pow_l(x, y);
+    if (strcmp(op, "min") == 0) return min_l(x, y);
+    if (strcmp(op, "max") == 0) return max_l(x, y);
+
+    return 0;
+}
+
+long eval(mpc_ast_t* t)
+{
+    if (strstr(t->tag, "integer"))
+    {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+    long x = eval(t->children[2]);
+
+    int arg_num = 0;
+    for (int i = 2; t->children[i] != t->children[t->children_num - 1]; i++)
+    {
+        arg_num++;
+    }
+
+    if (strcmp(op, "-") == 0 && arg_num == 1)
+    {
+        return x * -1;
+    }
+
+    int i = 3;
+    while(strstr(t->children[i]->tag, "expr"))
+    {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv)
 {
     mpc_parser_t* integer = mpc_new("integer");
@@ -42,7 +123,19 @@ int main(int argc, char** argv)
             integer : /-?\\d+/ ; \
             decimal : /-?\\d+\\.\\d+/ ; \
             number :  <decimal> | <integer> ; \
-            operator : '+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\" ; \
+            operator : '+' \
+                     | '-' \
+                     | '*' \
+                     | '/' \
+                     | '%' \
+                     | '^' \
+                     | \"add\" \
+                     | \"sub\" \
+                     | \"mul\" \
+                     | \"div\" \
+                     | \"mod\" \
+                     | \"min\" \
+                     | \"max\" ; \
             expr : <number> | '(' <operator> <expr>+ ')' ; \
             lispy : /^/ <operator> <expr>+ /$/ ; \
           ", integer, decimal, number, operator, expr, lispy);
@@ -58,7 +151,9 @@ int main(int argc, char** argv)
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, lispy, &r))
         {
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
+            // mpc_ast_print(r.output);
             mpc_ast_delete(r.output);
         }
         else
